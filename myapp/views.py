157c61c1f959e017924b8849d2ab5298cf823c25
unknown <cortobruzda@gmail.com>
2023-04-06 15:15:00 +0200

    Date commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import datetime
+
 
 # Create your views here.
+# Request -> response (request handler)
+
+
+def say_hello(request):
+    maintenant = datetime.datetime.now()
+    context = {'maintenant': maintenant}
+    return render(request, 'hello.html', context)
